30bd9c442ede03f23f9cace345bd770c80c346fb
neha-kumari-m <manojkumarr511@gmail.com>
2025-12-24 07:45:06 +0530

    Created a database connection and storing the users

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const userRouter = require('./routes/user.routes');
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDB = require('./config/db');
+connectToDB();
 
 const app = express();
 
@@ -12,3 +16,13 @@ app.use("/user",userRouter);
 app.listen(3000,()=> {
     console.log("Server is running on port 3000");
 })
+
+// Step 1: Check what changed
+// git status
+
+// 🔹 Step 2: Add only what you want
+// git add style.css
+// git add views/register.ejs
+
+// 🔹 Step 3: Commit
+// git commit -m "Improve input field opacity"
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user.model');
+const {body,validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 router.get('/register',(req,res)=>{
     res.render("register");
 });
 
-router.post('/register',(req,res)=>{
-    res.send(req.body);
+router.post('/register',
+    body('username').trim().isLength({min:5}),
+    body('email').trim().isEmail().isLength({min:13}),
+    body('password').trim().isLength({min:3}),
+    async (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:"Invalid data"
+            })
+        }
+        const {username,email,password} = req.body;
+        const hashPassword = await bcrypt.hash(password,10);
+        const newUser = await userModel.create({
+            username,
+            email,
+            password:hashPassword
+        }); 
+    res.json(newUser);
 })
 
 module.exports = router;

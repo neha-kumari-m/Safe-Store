b73879a4a76a9c72a31e28a784479e24ad86b6f3
neha-kumari-m <manojkumarr511@gmail.com>
2025-12-24 08:18:34 +0530

    Created a login page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connectToDB = require('./config/db');
 connectToDB();
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/user",userRouter);
+app.use(cookieParser());
 
 app.listen(3000,()=> {
     console.log("Server is running on port 3000");
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userModel = require('../models/user.model');
 const {body,validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/register',(req,res)=>{
     res.render("register");
@@ -28,6 +29,50 @@ router.post('/register',
             password:hashPassword
         }); 
     res.json(newUser);
+});
+
+router.get('/login',(req,res)=>{
+    res.render("login");
+});
+
+router.post('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:3}),
+    async (req,res)=>{
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:"Invalid data"
+            });
+        }
+        const {username,password} = req.body;
+
+        const user = await userModel.findOne({
+            username:username
+        })
+        if(!user){
+            return res.status(400).json({
+                message: "Username or password is incorrect"
+            });
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Username or password is incorrect"
+            })
+        }
+
+        const token = jwt.sign({
+            userId:user._id,
+            username:user.username,
+            email:user.email,
+        },
+            process.env.JWT_SECRET
+        );
+        res.cookie('token',token);
+        res.send("Logged In");
 })
 
 module.exports = router;
